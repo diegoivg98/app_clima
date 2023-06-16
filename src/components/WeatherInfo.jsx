4ffbc448b1ebfac753e94d5858adf1042a7e6d44
Diego Vistoso <diegoivg98@gmail.com>
2023-06-16 16:55:40 -0400

    agregar iconos a su respectivo span y cambios en los estilos

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,5 +1,8 @@
+import { GiThermometerHot, GiThermometerCold } from "react-icons/gi";
+import { WiHumidity } from "react-icons/wi"; 
+import { BiWind } from "react-icons/bi";
+
 const WeatherInfo = ({ weather }) => {
-  
   const WeatherTypes = [
     {
       type: "Clear",
@@ -44,26 +47,39 @@ const WeatherInfo = ({ weather }) => {
     <div className="max-w-sm mx-auto mt-10">
       {weather && (
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h2 className="text-2xl font-bold flex justify-center">{weather.name}</h2>
+          <h2 className="text-2xl font-bold flex justify-center">
+            {weather.name}
+          </h2>
           <div className="mt-3">
-            <p>
-              <span className="font-bold">Temperatura:</span>{" "}
+            <p className="flex items-center">
+              <span className="font-bold mr-2">
+                {Math.round(weather.main.temp) - 273 > 20 ? (
+                  <GiThermometerHot className="inline-block align-text-top" />
+                ) : (
+                  <GiThermometerCold className="inline-block align-text-top" />
+                )}
+              </span>
+              <span className="font-bold">Temperatura: </span>{" "}
               {/* Convertir Kelvin a Celsius */}
               {Math.round(weather.main.temp) - 273}°C
             </p>
-            <p>
-              <span className="font-bold">Humedad:</span>{" "}
-              {weather.main.humidity}%
+            <p className="flex items-center">
+              <span className="font-bold mr-2">
+                <WiHumidity className="inline-block align-text-top" />
+              </span>
+              <span className="font-bold">Humedad: </span> { weather.main.humidity }%
             </p>
-            <p>
-              <span className="font-bold">Viento:</span>{" "}
-              {weather.wind.speed} km/h
+            <p className="flex items-center">
+              <span className="font-bold mr-2">
+                <BiWind className="inline-block align-text-top" />
+              </span>
+              <span className="font-bold">Viento: </span> { weather.wind.speed } km/h
             </p>
             {weather.weather[0].main && (
               <img
-                src={getWeatherImage(weather.weather[0].main)}
-                alt={weather.weather[0].main}
-                className="mt-4"
+                src={ getWeatherImage(weather.weather[0].main) }
+                alt={ weather.weather[0].main }
+                className="mt-4 mx-auto"
               />
             )}
           </div>
