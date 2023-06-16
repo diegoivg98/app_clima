bf35255d5fcd44441872403e91cc0a0518ec5651
Diego Vistoso <diegoivg98@gmail.com>
2023-06-16 17:37:04 -0400

    cambio en el bg de WeatherInfo

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -46,7 +46,7 @@ const WeatherInfo = ({ weather }) => {
   return (
     <div className="max-w-sm mx-auto mt-10">
       {weather && (
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <h2 className="text-2xl font-bold flex justify-center">
             {weather.name}
           </h2>
@@ -61,7 +61,7 @@ const WeatherInfo = ({ weather }) => {
               </span>
               <span className="font-bold">Temperatura: </span>{" "}
               {/* Convertir Kelvin a Celsius */}
-              {Math.round(weather.main.temp) - 273}°C
+              {" "}{Math.round(weather.main.temp) - 273}°C
             </p>
             <p className="flex items-center">
               <span className="font-bold mr-2">
