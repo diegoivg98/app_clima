54d5ea5f479cce2fa0e5a5b297946b2a4fdeef3f
Diego Vistoso <diegoivg98@gmail.com>
2023-06-16 21:33:56 -0400

    agregar margenes para espaciado en los resultados

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -45,48 +45,47 @@ const WeatherInfo = ({ weather }) => {
 
   return (
     <div className="max-w-sm mx-auto mt-10">
-      {weather && (
-        <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h2 className="text-2xl font-bold flex justify-center">
-            {weather.name}
-          </h2>
-          <div className="mt-3">
-            <p className="flex items-center">
-              <span className="font-bold mr-2">
-                {Math.round(weather.main.temp) - 273 > 20 ? (
-                  <GiThermometerHot className="inline-block align-text-top" />
-                ) : (
-                  <GiThermometerCold className="inline-block align-text-top" />
-                )}
-              </span>
-              <span className="font-bold">Temperatura: </span>{" "}
-              {/* Convertir Kelvin a Celsius */}
-              {" "}{Math.round(weather.main.temp) - 273}°C
-            </p>
-            <p className="flex items-center">
-              <span className="font-bold mr-2">
-                <WiHumidity className="inline-block align-text-top" />
-              </span>
-              <span className="font-bold">Humedad: </span> { weather.main.humidity }%
-            </p>
-            <p className="flex items-center">
-              <span className="font-bold mr-2">
-                <BiWind className="inline-block align-text-top" />
-              </span>
-              <span className="font-bold">Viento: </span> { weather.wind.speed } km/h
-            </p>
-            {weather.weather[0].main && (
-              <img
-                src={ getWeatherImage(weather.weather[0].main) }
-                alt={ weather.weather[0].main }
-                className="mt-4 mx-auto"
-              />
-            )}
-          </div>
+    {weather && (
+      <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-2xl font-bold flex justify-center">
+          {weather.name}
+        </h2>
+        <div className="mt-3">
+          <p className="flex items-center mb-2">
+            <span className="font-bold">
+              {Math.round(weather.main.temp) - 273 > 20 ? (
+                <GiThermometerHot className="inline-block align-text-top" />
+              ) : (
+                <GiThermometerCold className="inline-block align-text-top" />
+              )}
+            </span>
+            <span className="font-bold mr-1 text-lg">Temperatura: </span>
+            {Math.round(weather.main.temp) - 273} °C
+          </p>
+          <p className="flex items-center mb-2">
+            <span className="font-bold">
+              <WiHumidity className="inline-block align-text-top" />
+            </span>
+            <span className="font-bold mr-1 text-lg">Humedad: </span> {weather.main.humidity}%
+          </p>
+          <p className="flex items-center mb-2">
+            <span className="font-bold">
+              <BiWind className="inline-block align-text-top" />
+            </span>
+            <span className="font-bold mr-1 text-lg">Viento:</span>{weather.wind.speed} km/h
+          </p>
+          {weather.weather[0].main && (
+            <img
+              src={getWeatherImage(weather.weather[0].main)}
+              alt={weather.weather[0].main}
+              className="mt-4 mx-auto"
+            />
+          )}
         </div>
-      )}
-    </div>
-  );
+      </div>
+    )}
+  </div>
+);
 };
 
 export default WeatherInfo;
